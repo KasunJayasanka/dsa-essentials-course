66c4aa563e388e8cdbbef9e019da82c0bac13296
Kasun Jayasanka <97845543+KasunJayasanka@users.noreply.github.com>
2023-07-12 16:55:33 +0530

    Create Array 03-Linear Search.cpp

---
+++ MyWorks/Array 03-Linear Search.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int linear_search(int arr[],int n,int key){
+
+    for(int i=0;i<n;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[]={5,6,7,8,9};
+    int sizeArr=sizeof(arr)/sizeof(int);
+
+    int key;
+    cin>>key;
+
+    int index=linear_search(arr,sizeArr,key);
+
+    if(index!=-1){
+        cout<<key<<" is present at index "<<index<<endl;
+    }
+    else{
+        cout<<key<<" is not found"<<endl;
+    }
+
+    return 0;
+}
