da8a85b8085cd6ebb4e6498bd5cd22255d9061d7
Kasun Jayasanka <officialblackknightcoders@gmail.com>
2024-01-13 21:36:09 +0530

    Create Optimised  Bubble Sort.cpp

---
+++ MyWorks/Sorting/Optimised  Bubble Sort.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> optimizedBubbleSort(vector<int> v){
+    // your code  goes here
+    int n = v.size();
+    bool swapped;
+
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (v[j] > v[j + 1]) {
+                std::swap(v[j], v[j + 1]);
+                swapped = true;
+            }
+        }
+
+        // If no swaps were made, the vay is already sorted
+        if (!swapped) {
+            break;
+        }
+    }
+
+    return v;
+}
