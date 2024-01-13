b475f8e4889cdb41a01582385fea403ed29c5ba6
Kasun Jayasanka <officialblackknightcoders@gmail.com>
2024-01-13 23:28:44 +0530

    Update and rename Bubble Sort.cpp to bubble_sort.cpp

--- MyWorks/Sorting/bubble_sort.cpp
+++ MyWorks/Sorting/bubble_sort.cpp
@@ -1,6 +1,6 @@
 using namespace std;
 
-void bubblesort(int arr[],int size){
+void bubble_sort(int arr[],int size){
     for(int times=1;times<size-1;times++){
         for(int j=0;j<=size-1-times;j++){
             if(arr[j]>arr[j+1]){
@@ -14,7 +14,7 @@ int main() {
     
     int arr[]= {-3,-2,-4,-9,1,5,6,7,19,-5};
     int size = sizeof(arr)/sizeof(arr[0]);
-    bubblesort(arr,size);
+    bubble_sort(arr,size);
     
     for(auto x:arr){
         cout << x << ",";
