6bd6728c92438641c46a419ce4cebe6ea99ced86
Kasun Jayasanka <officialblackknightcoders@gmail.com>
2024-01-13 23:29:47 +0530

    Update and rename Optimised  Bubble Sort.cpp to optimised _bubble_sort.cpp

--- MyWorks/Sorting/optimised _bubble_sort.cpp
+++ MyWorks/Sorting/optimised _bubble_sort.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> optimizedBubbleSort(vector<int> v){
+vector<int> optimized_bubble_sort(vector<int> v){
     // your code  goes here
     int n = v.size();
     bool swapped;
