8fc3e5e599568a922521c9f8d3c4a3c24ca6da3c
Kasun Jayasanka <officialblackknightcoders@gmail.com>
2024-01-13 15:05:06 +0530

    Create Bubble Sort.cpp

---
+++ MyWorks/Sorting/Bubble Sort.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+
+void bubblesort(int arr[],int size){
+    for(int times=1;times<size-1;times++){
+        for(int j=0;j<=size-1-times;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+int main() {
+    
+    int arr[]= {-3,-2,-4,-9,1,5,6,7,19,-5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    bubblesort(arr,size);
+    
+    for(auto x:arr){
+        cout << x << ",";
+    }
+
+    return 0;
+}
