116882b995d92094e0e5af9ed502d07a0bd6c1d0
Kasun Jayasanka <officialblackknightcoders@gmail.com>
2024-01-15 22:32:29 +0530

    Update inbuilt_sort.cpp

--- MyWorks/Sorting/inbuilt_sort.cpp
+++ MyWorks/Sorting/inbuilt_sort.cpp
@@ -2,11 +2,19 @@
 #include <algorithm>
 using namespace std;
 
+bool compare(int a,int b){
+    return a<b;
+}
+
 int main() {
     
     int arr[]= {-3,-2,-4,-9,1,5,6,7,19,-5};
     int size = sizeof(arr)/sizeof(arr[0]);
-    sort(arr,arr+size);
+    
+    
+    reverse(arr,arr+size); //dsecnding
+    sort(arr,arr+size,compare); //ascending using comparator
+    sort(arr,arr+size,greater<int>()); //dsecnding
     
     for(auto x:arr){
         cout << x << " ";
